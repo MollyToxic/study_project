01120ecaeb77f867954ee5dd4669c25055a703ff
MollyToxic <natalazdanova14887@gmail.com>
2024-05-08 16:25:45 +0300

    todo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+// const data = [
+//   {
+//     id: 'wefw23',
+//     title: 'Finish the essay collaboration',
+//     isCompleted: false,
+//   },
+//   {
+//     id: 'wefw23232',
+//     title: 'Read next chapter of the book',
+//     isCompleted: true,
+//   },
+//   {
+//     id: 'wefw2qwefcev3',
+//     title: 'Send the finished assignment',
+//     isCompleted: false,
+//   },
+// ]
+
+
+
+
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => response.json())
+      .then((json) => setTodos(json));
+  }, [])
+
+
+
+  function addTodo(title) {
+    setTodos(prev => [
+      {
+        id: new Date(),
+        title,
+        isCompleted: false,
+      },
+      ...prev,
+    ])
+    setTitle('')
+  }
+
+  function removeTodo(id) {
+    setTodos([...todos].filter(t => t.id !== id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container-input'>
+        <input type='text' onChange={e => setTitle(e.target.value)} value={title} />
+        <button onClick={() => addTodo(title)}>add</button>
+      </div>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <input className='check' type="checkbox" defaultChecked={todo.сompleted}></input>
+            {todo.title}
+            <button onClick={() => removeTodo(todo.id)}>X</button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
